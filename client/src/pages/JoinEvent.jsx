9faf13e921dccb4cd72a287fae497a649c04e9e6
Mesadou <orylee@uw.edu>
2026-04-28 14:51:58 -0700

    Added more styling to JoinEvent.css. Description spacing, Initiation Header, Join group button.

--- client/src/pages/JoinEvent.jsx
+++ client/src/pages/JoinEvent.jsx
@@ -6,7 +6,7 @@ function JoinEvent() {
 
       <section className="invitation-card">
         {/*Invitation from */}
-        <div>
+        <div className="invite-group">
           <img />
           <h1>*Person* has invited you to *Game Night*</h1>
         </div>
@@ -18,7 +18,10 @@ function JoinEvent() {
             <h1>Event/Game Name</h1>
           </div>
 
-          <p>Event/Game Description</p>
+          <div className="event-desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit feugiat purus, elementum ultricies sem dapibus porttitor. Fusce hendrerit nisi risus, non pellentesque ex varius eget. Sed elementum fringilla sapien, vitae aliquam lorem blandit vitae. Vivamus non risus volutpat ex consectetur varius in sit amet massa. Vestibulum quis </p>
+          </div>
+          
           
           <div className="event-group">
             <img className="group-icons" />
