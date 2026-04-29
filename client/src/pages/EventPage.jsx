067669fd727022c38ff40aff23a82b7b34aad46d
Rylee <orylee@uw.edu>
2026-04-29 15:16:32 -0700

    Added stuff to the EventPage.jsx and .css

--- client/src/pages/EventPage.jsx
+++ client/src/pages/EventPage.jsx
@@ -1,5 +1,4 @@
 import "./EventPage.css"
-import "../components/EventCard.css"
 import EventCard from '../components/EventCard';
 
 function EventPage() {
@@ -12,35 +11,32 @@ function EventPage() {
       <section className="event-chat">
         {/*Group Name line */}
         <div className="event-name">
-          <h1>John Smith's Group</h1>
+          <h1 className="group-name">John Smith's Group</h1>
         </div>
 
-
+        <section className="event-box">
+         <div>
+            <EventCard
+            title="Event/Game Name"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit feugiat purus, elementum ultricies sem dapibus porttitor. Fusce hendrerit nisi risus, non pellentesque ex varius eget. Sed elementum fringilla sapien, vitae aliquam lorem blandit vitae. Vivamus non risus volutpat ex consectetur varius in sit amet massa. Vestibulum quis "
+            imageSrc=""
+            members={[
+              {id: 1, name: "Rylee", avatar: "https://i.pravatar.cc/150?img=1"},
+              {id: 2, name: "Darty", avatar: "https://i.pravatar.cc/150?img=2"},
+              {id: 3, name: "Jordan", avatar: "https://i.pravatar.cc/150?img=3"}
+            ]}
+         />
+          <p># Votes [0/9]</p>
+            <button>No</button>
+           <button>Yes</button>
+         </div>
+          
+          <div className="type-bar">
+            <p>Enter Text bottom bar</p>
+          </div>
         
-
-        <div>
-          <EventCard
-          title="Event/Game Name"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit feugiat purus, elementum ultricies sem dapibus porttitor. Fusce hendrerit nisi risus, non pellentesque ex varius eget. Sed elementum fringilla sapien, vitae aliquam lorem blandit vitae. Vivamus non risus volutpat ex consectetur varius in sit amet massa. Vestibulum quis "
-          imageSrc=""
-          members={[
-            {id: 1, name: "Rylee", avatar: "https://i.pravatar.cc/150?img=1"},
-            {id: 2, name: "Darty", avatar: "https://i.pravatar.cc/150?img=2"},
-            {id: 3, name: "Jordan", avatar: "https://i.pravatar.cc/150?img=3"}
-          ]}
-        />
-        <p># Votes [0/9]</p>
-          <button>No</button>
-          <button>Yes</button>
-        </div>
-          
-          
-          
-        
-
-        <div className="type-bar">
-          <p>Enter Text bottom bar</p>
-        </div>
+        </section>
+ 
       </section>
 
       <section className="event-lists">
